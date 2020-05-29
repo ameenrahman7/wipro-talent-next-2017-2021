3d92595dd6fef19d3779218fc809a1643fb7f3bf
ameenrahman7 <65948104+ameenrahman7@users.noreply.github.com>
2020-05-29 22:47:49 +0530

    Add files via upload

---
+++ Day3/CmdLineArg1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class CmdLineArg1
+{
+    public static void main(String [] args)
+    {
+        System.out.print("Welcome " +args[0]);
+    }
+}
---
+++ Day3/Printto10.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Printto10 
+{
+    public static void main(String[] args) 
+    {
+        for(int i = 1; i <= 10; i++) 
+        {
+            System.out.print(" " + i);
+        }
+    }
+}
